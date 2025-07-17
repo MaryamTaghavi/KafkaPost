d0fb1c3a18b22e3d673d207202af5b203a6e23d2
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-07-17 13:46:15 +0330

    createPostAggregate

---
+++ SM-Post/Post.Cmd.Domain/Aggregates/PostAggregate.cs
@@ -0,0 +1,55 @@
+﻿using CQRS.Core.Domain;
+using Microsoft.AspNetCore.Builder;
+using Post.Common.Api.Events;
+
+namespace Post.Cmd.Domain.Aggregates;
+
+public class PostAggregate : AggregateRoot
+{
+    private bool _active;
+    private string author;
+
+    private readonly Dictionary<Guid, Tuple<string, string>> _comments = new();
+
+    // TODO :
+
+    // public bool Active
+    // {
+    //   get {} ; set {} ;
+    // }
+
+    // تفاوت تعریف متد در دو حالت این است که در حالت بالایی
+    // کامپایلر یک فیلد پشتیبان پنهان میسازد که ما به آن دسترسی
+    // نداریم و نمیتوانیم آن رو کنترل کنیم
+    // ولی در حالت پایینی کاملا به آن مسلط و امکان تغییر آن را داریم
+
+    // set
+    // {
+    //      if (value != _active)
+    //      {
+    //          Console.WriteLine("Active changed!");
+    //          _active = value;
+    //      }
+    //}
+
+    public bool Active
+    {
+        get => _active; set => _active = value;
+    }
+
+    public PostAggregate()
+    {
+
+    }
+
+    public PostAggregate(Guid id, string author, string message)
+    {
+        RaiseEvent(new PostCreatedEvent
+        {
+            Id = id,
+            Author = author,
+            Message = message,
+            DatePosted = DateTime.Now
+        });
+    }
+}
--- SM-Post/Post.Common.Api/Events/PostCreatedEvent.cs
+++ SM-Post/Post.Common.Api/Events/PostCreatedEvent.cs
@@ -7,7 +7,10 @@ public class PostCreatedEvent : BaseEvent
     public PostCreatedEvent() : base(nameof(PostCreatedEvent))
     {
     }
+
     public string Author { get; set; }
+
     public string Message { get; set; }
+
     public DateTime DatePosted { get; set; }
 }
